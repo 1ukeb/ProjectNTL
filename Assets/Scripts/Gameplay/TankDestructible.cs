6ec417c0258a0a2c710e684cffc836622dea6264
StvnmHHgit conifg --global user.email stevenhill26.fun@gmail.com <stevenhill26.fun@gmail.com>
2021-06-26 01:18:19 -0700

    gameplay explosion

--- Assets/Scripts/Gameplay/TankDestructible.cs
+++ Assets/Scripts/Gameplay/TankDestructible.cs
@@ -4,10 +4,19 @@ namespace NTL.Gameplay
 {
     public class TankDestructible : Destructible
     {
+
+        [SerializeField]
+        private GameObject explosionPrefab;
         public override bool IsValidCollision(Collider col)
         {
             // return if is bullet tag and bullet is not from this tank
             return base.IsValidCollision(col);
         }
+
+        public override void Destruct()
+      {
+          Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+      }
+        
     }
 }
